0dc372fffdc3c803bea28cf295de1c9115d3e189
Phan Van Tan <tana2k53nvt@gmail.com>
2026-04-09 11:28:17 +0700

    agent soap

--- src/tool/soap_ai/main.py
+++ src/tool/soap_ai/main.py
@@ -2,17 +2,21 @@ import os
 import json
 from openai import OpenAI
 from dotenv import load_dotenv
-from Lab5_C401_D6.src.tool.soap_ai.utils.formatter import validate_soap
+from tool.soap_ai.utils.formatter import validate_soap
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 load_dotenv()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+PROVIDER = os.getenv("LLM_PROVIDER")
+API_KEY = os.getenv("LLM_API_KEY")
+MODEL = os.getenv("LLM_MODEL")
 
 # load prompt
-with open("prompts/system_prompt.txt", "r", encoding="utf-8") as f:
+with open(os.path.join(BASE_DIR, "prompts/system_prompt.txt"), "r", encoding="utf-8") as f:
     system_prompt = f.read()
 
 # load few-shot
-with open("few_shot/examples.json", "r", encoding="utf-8") as f:
+with open(os.path.join(BASE_DIR, "few_shot/examples.json"), "r", encoding="utf-8") as f:
     examples = json.load(f)
 
 def build_messages(user_input):
@@ -29,8 +33,22 @@ def build_messages(user_input):
 def generate_soap(user_input):
     messages = build_messages(user_input)
 
+    if PROVIDER == "openai":
+        
+        client = OpenAI(api_key=API_KEY)
+
+    elif PROVIDER == "openrouter":
+        
+        client = OpenAI(
+            api_key=API_KEY,
+            base_url="https://openrouter.ai/api/v1"
+        )
+
+    else:
+        return "❌ Provider không hỗ trợ"
+
     response = client.chat.completions.create(
-        model="gpt-5-mini",
+        model=MODEL,
         messages=messages,
         temperature=0.3
     )
@@ -39,20 +57,20 @@ def generate_soap(user_input):
 
     if not validate_soap(result):
         return "⚠️ Output không đúng format SOAP!"
-
     return result
 
-
+def load_input_from_file(file_name):
+    path = os.path.join(BASE_DIR, file_name)
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+    
 if __name__ == "__main__":
-    print("Nhập hội thoại (gõ exit để thoát)\n")
+    
+    
+        input_text = load_input_from_file("test_input.txt")
+        
 
-    while True:
-        user_input = input("Bạn:\n")
-
-        if user_input.lower() == "exit":
-            break
-
-        output = generate_soap(user_input)
+        output = generate_soap(input_text)
 
         print("\n=== SOAP ===")
         print(output)
